6f376ac456e6ad2275a6f3327ef4117e42c1d42e
anof1r <denchik.anofriev@gmail.com>
2023-02-27 11:30:41 +0300

    Auto-generated commit [ci skip]

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  api_key: 'pUmY7lkTS16mVDzexV4reWg4IiDiD742bqc9o9tb'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  api_key: 'pUmY7lkTS16mVDzexV4reWg4IiDiD742bqc9o9tb - prodApi'
-};
